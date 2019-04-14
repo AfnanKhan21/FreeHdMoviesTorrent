a162908fc86f7bc27927e3d40866e2bb0bade500
Afnan khan <afakhan413@gmail.com>
2019-04-15 01:26:20 +0500

    first layout

--- FreeHdMoviesTorrent/App_Start/BundleConfig.cs
+++ FreeHdMoviesTorrent/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace FreeHdMoviesTorrent
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
